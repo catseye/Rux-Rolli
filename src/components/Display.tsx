f59f1264aa14b5577050cb00b3c81efba74354e3
Chris Pressey <cpressey@catseye.tc>
2022-04-23 17:52:15 +0100

    Expand Display component to display composite configurations.

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -9,7 +9,17 @@ interface DisplayProps {
 export function Display(props: DisplayProps) {
   if (props.configuration.type === "text") {
     return (
-        <pre>{ props.configuration.contents }</pre>
+      <pre>{ props.configuration.contents }</pre>
+    );
+  } else if (props.configuration.type === "stack") {
+    return (
+      <pre>{ props.configuration.contents }</pre>
+    );
+  } else if (props.configuration.type === "composite") {
+    return (
+      <div>
+        {props.configuration.contents.map((c) => <Display configuration={c} />)}
+      </div>
     );
   } else {
     return (
