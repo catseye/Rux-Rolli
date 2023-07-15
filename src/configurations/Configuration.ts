2a93235d2ce1f4aaca867cc1938c1e71b11e72e4
Chris Pressey <cpressey@catseye.tc>
2023-07-15 02:28:11 +0100

    Helper functions to make semantics a little more concise.

--- src/configurations/Configuration.ts
+++ src/configurations/Configuration.ts
@@ -22,6 +22,24 @@ export function getChildren(c: Composite): Array<Configuration> {
   return c.contents;
 }
 
+export function getTextChild(c: Configuration, index: number): Text | null {
+  if (c.type !== 'composite') return null;
+  const child = c.contents[index];
+  return (child.type === 'text') ? child : null;
+}
+
+export function getPlayfieldChild(c: Configuration, index: number): Playfield | null {
+  if (c.type !== 'composite') return null;
+  const child = c.contents[index];
+  return (child.type === 'playfield') ? child : null;
+}
+
+export function getStackChild(c: Configuration, index: number): Stack | null {
+  if (c.type !== 'composite') return null;
+  const child = c.contents[index];
+  return (child.type === 'stack') ? child : null;
+}
+
 export function mapComposite(c: Composite, f: (x: Configuration, index: number) => any): any {
   return c.contents.map(f);
 }
--- src/semantics/Thencemuffin.ts
+++ src/semantics/Thencemuffin.ts
@@ -1,7 +1,7 @@
-import { Configuration, newComposite, getChildren } from "../configurations/Configuration";
-import { Text, newText, newRange, getString, getCursors, moveCursor as moveRange } from "../configurations/Text";
-import { Stack, newStack, push, pop } from "../configurations/Stack";
-import { Playfield, newPlayfield, setCursor, put, moveCursor } from "../configurations/Playfield";
+import { Configuration, newComposite, getTextChild, getPlayfieldChild, getStackChild } from "../configurations/Configuration";
+import { newText, newRange, getString, getCursors, moveCursor as moveRange } from "../configurations/Text";
+import { newStack, push, pop } from "../configurations/Stack";
+import { newPlayfield, setCursor, put, moveCursor } from "../configurations/Playfield";
 import { newCursor } from "../configurations/Cursor";
 import { Semantics } from "../semantics";
 
@@ -20,21 +20,17 @@ export const Thencemuffin: Semantics = {
     ]);
   },
   next: function(configuration: Configuration) {
-    if (configuration.type !== 'composite') return ['halt', configuration];
-    const children = getChildren(configuration);
-    if (children[0].type !== 'text') return ['halt', configuration];
-    const text: Text = children[0];
-    if (children[1].type !== 'playfield') return ['halt', configuration];
-    const pf: Playfield = children[1];
-    if (children[2].type !== 'stack') return ['halt', configuration];
-    const stack: Stack = children[2];
+    const text = getTextChild(configuration, 0);
+    const pf = getPlayfieldChild(configuration, 1);
+    const stack = getStackChild(configuration, 2);
+    if (!text || !pf || !stack) return ['halt', configuration];
 
     let newText = moveRange(text, 0, 1);
     let cursor = getCursors(newText)[0];
     if (cursor.index >= getString(newText).length) return ['halt', configuration];
     let char = getString(newText).charAt(cursor.index);
 
-    let newStack: Stack;
+    let newStack;
     if (char === 'S') {
       const tmp = pop(stack);
       newStack = tmp !== null ? tmp : stack;
@@ -42,7 +38,7 @@ export const Thencemuffin: Semantics = {
       newStack = push(stack, "A");
     }
 
-    let newPf: Playfield = pf;
+    let newPf = pf;
     newPf = put(newPf, 2, 1, char);
     newPf = moveCursor(newPf, "IP", 1, 0);
     return ['next', newComposite([newText, newPf, newStack])];
